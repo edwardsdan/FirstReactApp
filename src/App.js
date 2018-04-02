590d74ffe8ac1edbee0af4a2f6bf0c6eaa189557
DanE <edwardsdan001@gmail.com>
2018-04-01 20:17:59 -0400

    Added some radium styling

--- src/App.js
+++ src/App.js
@@ -57,7 +57,11 @@ class App extends Component {
       font: 'inherit',
       border: '1px solid blue',
       padding: '8px',
-      cursor: 'pointer'
+      cursor: 'pointer',
+      ':hover': {
+        backgroundColor: 'lightgreen',
+        color: 'black'
+      }
     }
 
     let people = null
@@ -80,6 +84,10 @@ class App extends Component {
       );
 
       style.backgroundColor = 'red';
+      style[':hover'] = {
+          backgroundColor: 'salmon',
+          color: 'black'
+      }
     }
 
     let classes = [];
