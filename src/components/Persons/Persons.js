a28f70a68b7b53a46160b7df518a21376732b717
DanE <edwardsdan001@gmail.com>
2018-04-09 01:02:59 -0400

    added method overrides

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,7 +1,7 @@
-import React, {Component} from 'react';
+import React, {PureComponent} from 'react';
 import Person from './Person/Person';
 
-class Persons extends Component {
+class Persons extends PureComponent {
 
     constructor(props) {
         super(props);
@@ -20,6 +20,26 @@ class Persons extends Component {
 
     componentDidMount() {
     console.log('[Persons.js] inside componentDidMount()');
+    }
+
+    componentWillReceiveProps(nextProps) {
+        console.log('[UPDATE Persons.js] inside componentWillReceiveProps()', nextProps); 
+    }
+
+    // shouldComponentUpdate(nextProps, nextState) {
+    //     console.log('[UPDATE Persons.js] inside shouldComponentUpdate()', nextProps, nextState);
+    //     return nextProps.persons !== this.props.persons ||
+    //     nextProps.changed !== this.props.changed ||
+    //     nextProps.clicked !== this.props.clicked;
+    //     // return true;
+    // }
+
+    componentWillUpdate(nextProps, nextState) {
+        console.log('[UPDATE Persons.js] inside componentWillUpdate()', nextProps, nextState);
+    }
+
+    componentDidUpdate() {
+        console.log('[UPDATE Persons.js] inside componentDidUpdate()');
     }
 
     render() {
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,9 +1,9 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import classes from './App.css';
 import Persons from '../components/Persons/Persons';
 import Cockpit from '../components/Cockpit/Cockpit';
 
-class App extends Component {
+class App extends PureComponent {
 
   constructor(props) {
     super(props);
@@ -22,6 +22,20 @@ class App extends Component {
 
   componentDidMount() {
     console.log('[App.js] inside componentDidMount()');
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log('[UPDATE App.js] inside shouldComponentUpdate()');
+  //   return nextState.people !== this.state.people ||
+  //   nextState.showPeople !== this.state.showPeople;
+  // }
+
+  componentWillUpdate(nextProps, nextState) {
+    console.log('[UPDATE App.js] inside componentWillUpdate');
+  }
+
+  componentDidUpdate() {
+    console.log('[UPDATE App.js] inside componentDidUpdate()');
   }
 
   // Declare state (class-wide variable) that, when updated,
@@ -84,6 +98,7 @@ class App extends Component {
 
     return (
         <div className={classes.App}>
+          <button onClick={() => {this.setState({showPeople: true})}}>Show People</button>
           <Cockpit
             appTitle={this.props.title}
             showPeople={this.state.showPeople}
